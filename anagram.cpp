b3dcce4e03547aa56c25d35c5ec1f79410dac57b
Godard H.I <42699475+godardhi@users.noreply.github.com>
2023-07-04 10:52:16 +0200

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -46,12 +46,12 @@ void procedure1(vector<string>& dictionnaire)
 {
 	for(int i=0; i<dictionnaire.size(); i++)
 	{
-		cout<<"*"<<dictionnaire[i]<<endl;
+		cout<<"*"<<dictionnaire[i]<<endl; // A certain word is marked with *
 		for(int j=i+1; j<dictionnaire.size(); j++)
 		{
 			if(anagramme_check(dictionnaire[i],dictionnaire[j])==true)
 			{
-				cout<<"-"<<dictionnaire[j]<<endl;
+				cout<<"-"<<dictionnaire[j]<<endl; // and its corresponding anagrams are marked with -
 			}
 		}
 	}
@@ -64,7 +64,7 @@ int main()
 	procedure0(dictionnaire);
 		
 	// Une fonction qui vérifie si chacun des mots contenus dans le fichier mots.txt à un anagramme ou non \\ A function that checks if every word in the vector has an anagram and if so 
-	// + affiche et enregristre les anagrammes dans un fichier anagrammes.txt. \\ it promp it on the console and saves those possesing anagrams and their anagrams in a text file.
+	// + affiche et enregristre les anagrammes dans un fichier anagrammes.txt. \\ it promps it on the console and saves those possesing anagrams and their anagrams in a text file.
 	procedure1(dictionnaire);
 	
 	return 0;
